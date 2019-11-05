d65286d5cf95a6d4ed5a90ede78e7409db8b14f6
milankl <milankloewer@gmx.de>
2019-11-05 08:54:33 +0000

    Figure headers

--- src/pie_charts_mode.py
+++ src/pie_charts_mode.py
@@ -4,15 +4,15 @@ import matplotlib.pyplot as plt
 import matplotlib
 
 ## read data
-path = "/local/home/kloewer/dbx/Dropbox/CarbonFootprintEGU/data/"
+path = "Dropbox/CarbonFootprintEGU/data/"
 df = pd.read_excel(path+"data_processed.xlsx")
 
-dist = df["Distance to Vienna"].to_numpy()
+dist = np.array(df["Distance to Vienna"])
 n = len(dist)
 
 ## SCENARIO REALISTIC
-S1 = df["per country Realistic"].to_numpy()
-N = df["Country"].to_numpy()
+S1 = np.array(df["per country Realistic"])
+N = np.array(df["Country"])
 
 S1[np.isnan(S1)] = 0
 
@@ -21,7 +21,7 @@ C1sh = sum([S1[i] for i in range(n) if dist[i] >= 700 and dist[i] < 1500])/sum(S
 C1ra = sum([S1[i] for i in range(n) if dist[i] < 700])/sum(S1)*100
 
 ## SCENARIO EUROPE BY RAIL
-S2 = df["per country Rail"].to_numpy()
+S2 = np.array(df["per country Rail"])
 
 S2[np.isnan(S2)] = 0
 C2lh = sum([S2[i] for i in range(n) if dist[i] > 1500])/sum(S2)*100
@@ -38,7 +38,7 @@ fig,(ax1,ax2) = plt.subplots(1,2,figsize=(12,4))
 wedges1, texts1, autotexts1 = ax1.pie([C1lh,C1sh,C1ra],labels=["Long-haul","Short-haul","Rail"],autopct="%.1f%%",colors=colors)
 plt.setp(autotexts1, size=8, weight="bold", color="white")
 ax1.axis('equal')
-ax1.set_title("a) Travel to EGU19, total %d tC02e" % sum(S1),loc="left",weight="bold")
+ax1.set_title("a) Breakdown by mode of transport, total %d tC02e" % sum(S1),loc="left",weight="bold")
 
 # RAIL
 wedges2, texts2, autotexts2 = ax2.pie([C2lh,C2ra],labels=["Long-haul","Rail"],autopct="%.1f%%",colors=colors)
